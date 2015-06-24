772663d04c6e37666d172507a4f6cab4ad0bda0a
toru1055 <toru1055h@gmail.com>
2015-06-25 01:21:06 +0900

    add CorporatePerformance's database controller.

--- src/main/java/jp/thotta/ifinance/model/CorporatePerformance.java
+++ src/main/java/jp/thotta/ifinance/model/CorporatePerformance.java
@@ -1,5 +1,12 @@
 package jp.thotta.ifinance.model;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.util.Map;
+import java.util.HashMap;
+
 /**
  * 通期の企業業績クラス.
  * {@link DBModel}を継承し、業績テーブルとのアクセスも持つ
@@ -7,9 +14,9 @@ package jp.thotta.ifinance.model;
  * @author toru1055
  */
 public class CorporatePerformance implements DBModel {
-  int stockId; //pk
-  int settlingYear; // pk
-  int settlingMonth; // pk
+  public int stockId; //pk
+  public int settlingYear; // pk
+  public int settlingMonth; // pk
   public long salesAmount;
   public long operatingProfit;
   public long ordinaryProfit;
@@ -59,5 +66,90 @@ public class CorporatePerformance implements DBModel {
         debtWithInterest,
         capitalFund);
     return s;
+  }
+
+  /**
+   * 企業業績テーブル作成.
+   * @param c dbのコネクション
+   */
+  public static void createTable(Connection c) 
+    throws SQLException {
+    String sql = 
+      "CREATE TABLE corporate_performance(" +
+        "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+        "stock_id INT NOT NULL, " +
+        "settling_year INT NOT NULL, " +
+        "settling_month INT NOT NULL, " +
+        "sales_amount BIGINT, " +
+        "operating_profit BIGINT, " +
+        "ordinary_profit BIGINT, " +
+        "net_profit BIGINT, " +
+        "total_assets BIGINT, " +
+        "debt_with_interest BIGINT, " +
+        "capital_fund BIGINT, " +
+        "UNIQUE(stock_id, settling_year, settling_month)" +
+      ")";
+    c.createStatement().executeUpdate(sql);
+  }
+
+  /**
+   * 企業業績テーブルを削除.
+   * @param c dbのコネクション
+   */
+  public static void dropTable(Connection c) 
+    throws SQLException {
+    String sql = "DROP TABLE IF EXISTS corporate_performance";
+    c.createStatement().executeUpdate(sql);
+  }
+
+  /**
+   * Mapのデータを全てDBにInsertする.
+   * @param m モデルのmap
+   * @param c dbのコネクション
+   */
+  public static void insertMap(Map<String, CorporatePerformance> m, Connection c) 
+    throws SQLException {
+    String sqlFormat = 
+      "INSERT INTO corporate_performance(" +
+        "stock_id, settling_year, settling_month," +
+        "sales_amount, operating_profit, ordinary_profit, net_profit, " +
+        "total_assets, debt_with_interest, capital_fund" +
+      ") values(%4d, %4d, %2d, %d, %d, %d, %d, %d, %d, %d)";
+    Statement st = c.createStatement();
+    for(String k : m.keySet()) {
+      CorporatePerformance v = m.get(k);
+      st.executeUpdate(String.format(sqlFormat,
+            v.stockId, v.settlingYear, v.settlingMonth,
+            v.salesAmount, v.operatingProfit, v.ordinaryProfit, v.netProfit,
+            v.totalAssets, v.debtWithInterest, v.capitalFund));
+    }
+  }
+
+  /**
+   * テーブル内の全てのレコードをMapにして返す.
+   * @param c dbのコネクション
+   */
+  public static Map<String, CorporatePerformance> selectAll(Connection c)
+    throws SQLException {
+    Map<String, CorporatePerformance> m =
+      new HashMap<String, CorporatePerformance>();
+    String sql = "SELECT * FROM corporate_performance";
+    ResultSet rs = c.createStatement().executeQuery(sql);
+    while(rs.next()) {
+      int stockId = rs.getInt("stock_id");
+      int settlingYear = rs.getInt("settling_year");
+      int settlingMonth = rs.getInt("settling_month");
+      CorporatePerformance v = 
+        new CorporatePerformance(stockId, settlingYear, settlingMonth);
+      v.salesAmount = rs.getLong("sales_amount");
+      v.operatingProfit = rs.getLong("operating_profit");
+      v.ordinaryProfit = rs.getLong("ordinary_profit");
+      v.netProfit = rs.getLong("net_profit");
+      v.totalAssets = rs.getLong("total_assets");
+      v.debtWithInterest = rs.getLong("debt_with_interest");
+      v.capitalFund = rs.getLong("capital_fund");
+      m.put(v.getKeyString(), v);
+    }
+    return m;
   }
 }
--- src/test/java/jp/thotta/ifinance/model/DBModelTest.java
+++ src/test/java/jp/thotta/ifinance/model/DBModelTest.java
@@ -12,19 +12,22 @@ import java.util.HashMap;
 import java.io.IOException;
 import java.text.ParseException;
 
-import jp.thotta.ifinance.collector.yj_finance.StockPriceCollectorImpl;
+import jp.thotta.ifinance.collector.yj_finance.*;
 
 public class DBModelTest extends TestCase {
   Map<String, DailyStockPrice> stockTable;
+  Map<String, CorporatePerformance> performances;
   Connection c;
 
   protected void setUp() {
     stockTable = new HashMap<String, DailyStockPrice>();
+    performances = new HashMap<String, CorporatePerformance>();
     try {
       c = Database.getConnection();
       DailyStockPrice.dropTable(c);
       DailyStockPrice.createTable(c);
-
+      CorporatePerformance.dropTable(c);
+      CorporatePerformance.createTable(c);
     } catch(SQLException e) {
       e.printStackTrace();
     }
@@ -49,6 +52,46 @@ public class DBModelTest extends TestCase {
     } catch(SQLException e) {
       e.printStackTrace();
     } catch(ParseException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void testCorporatePerformance() {
+    SalesAmountCollectorImpl sac = new SalesAmountCollectorImpl();
+    sac.setStartPage(71);
+    OperatingProfitCollectorImpl oppc = new OperatingProfitCollectorImpl();
+    oppc.setStartPage(71);
+    OrdinaryProfitCollectorImpl orpc = new OrdinaryProfitCollectorImpl();
+    orpc.setStartPage(71);
+    NetProfitCollectorImpl npc = new NetProfitCollectorImpl();
+    npc.setStartPage(71);
+    TotalAssetsCollectorImpl tac = new TotalAssetsCollectorImpl();
+    tac.setStartPage(66);
+    DebtWithInterestCollectorImpl dic = new DebtWithInterestCollectorImpl();
+    dic.setStartPage(59);
+    CapitalFundCollectorImpl cfc = new CapitalFundCollectorImpl();
+    cfc.setStartPage(72);
+    try {
+      sac.append(performances);
+      oppc.append(performances);
+      orpc.append(performances);
+      npc.append(performances);
+      tac.append(performances);
+      dic.append(performances);
+      cfc.append(performances);
+    } catch(IOException e) {
+      e.printStackTrace();
+    }
+    assertTrue(performances.size() > 0);
+    // TODO: insertMapとselectAllのテストを書く
+    try {
+      CorporatePerformance.insertMap(performances, c);
+      Map<String, CorporatePerformance> m = CorporatePerformance.selectAll(c);
+      for(String k : m.keySet()) {
+        System.out.println(m.get(k));
+      }
+      assertTrue(m.size() > 0);
+    } catch(SQLException e) {
       e.printStackTrace();
     }
   }
